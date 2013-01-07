8341298a1ed4ff41474c28f0aa4fe6cd8aab0c3a
Reinhard <reinhard93@gmx.at>
2013-01-07 23:42:48 +0100

    testcase
    not working

--- CarRental/CarRental/Main.cpp
+++ CarRental/CarRental/Main.cpp
@@ -1,18 +1,93 @@
+///////////////////////////////////////////////////////////////////////////
+// Workfile : Main.cpp
+// Author : Reinhard Penn, Bernhard Selymes
+// Date : 02.01.2013
+// Description : Testdriver for CarRental
+///////////////////////////////////////////////////////////////////////////
+
 #include <iostream>
+#include <algorithm>
+#include <vld.h>
+#include "ICar.h"
+#include "CarRental.h"
+#include "Decorator.h"
+#include "Decorator_AirConditioner.h"
+#include "Decorator_Navi.h"
+#include "Decorator_Speedometer.h"
+#include "Decorator_Xenion.h"
+#include "SmallCar.h"
+#include "MiddleRangeCar.h"
+#include "PremiumCar.h"
+#include "SUV.h"
 
 using  namespace std;
 
-#include "ICar.h"
-#include "Decorator.h"
-#include "Decorator_AirConditioner.h"
-#include "SmallCar.h"
+
+void EmptyTestCase()
+{
+	cout << "Empty testcase with NULL pointer." << endl;
+
+	CarRental Rental;
+
+	Rental.Add(0);
+	Rental.GetAvailable("","");
+	Rental.GetReserved("","");
+	Rental.MoveToAvailable(0);
+	Rental.Reserve(0);
+	Rental.PrintAvailable(cout);
+	Rental.PrintReserved(cout);
+
+	cout << endl << endl;
+}
+
+void SingleTestCase()
+{
+	cout << "Testcase with single entry" << endl;
+
+	CarRental Rental;
+
+	ICar* VW = new SmallCar("VW","Golf");
+	ICar* MyCar = new Decorator_AirConditioner(VW);
+
+	cout << "Add ...";
+	Rental.Add(MyCar);
+	cout << "done" << endl;
+
+	cout << "GetAvailable ...";
+	TCarList list = Rental.GetAvailable("VW","Golf");
+	cout << "done" << endl;
+
+	cout << "Reserve ...";
+	for_each(list.begin(),list.end(),[&](ICar* m)
+	{
+		Rental.Reserve(m);
+	});	
+	cout << "done" << endl;
+
+	cout << "GetReserved ...";
+	Rental.GetReserved("VW","Golf");
+	cout << "done" << endl;
+
+	cout << "PrintReserved ...";
+	Rental.PrintReserved(cout);
+	cout << "done" << endl;
+
+	cout << "MoveToAvailable ...";	
+	for_each(list.begin(),list.end(),[&](ICar* m)
+	{
+		Rental.MoveToAvailable(m);
+	});	
+	cout << "done" << endl;	
+
+	cout << "PrintAvailable ...";
+	Rental.PrintAvailable(cout);
+	cout << "done" << endl;
+}
 
 int main()
 {
-	ConcreteCar* car = new SmallCar("Audi","A4");
-	ICar* airCondi = new Decorator_AirConditioner(car,3000);
-	airCondi->Print(cout);
-	cout << "Total Price: " << airCondi->GetPrice() << endl;
-	
+	EmptyTestCase();
+	SingleTestCase();
+
 	return 0;
 }
